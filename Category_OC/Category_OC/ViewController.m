f25e0f0b4ac02a3fb0d853d25fc537a31c313dea
lingaoo <lingaooli@gmail.com>
2018-08-20 15:54:51 +0800

    Commit
    Commit

--- Category_OC/Category_OC/ViewController.m
+++ Category_OC/Category_OC/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIColor+HEX.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    self.view.backgroundColor = [UIColor colorWithHexString:@"e3a3b3"];
+    
 }
 
 - (void)didReceiveMemoryWarning {
