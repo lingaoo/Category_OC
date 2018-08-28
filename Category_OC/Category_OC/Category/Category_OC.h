fea9ec45f97afdf882fcbf114a407552813a74c0
lingaoo <lingaooli@gmail.com>
2018-08-28 10:17:23 +0800

    Commit...
    Commit...

---
+++ Category_OC/Category_OC/Category/Category_OC.h
@@ -0,0 +1,27 @@
+//
+//  Category_OC.h
+//  Category_OC
+//
+//  Created by teamotto on 2018/8/27.
+//  Copyright © 2018年 soso. All rights reserved.
+//
+
+//  如果出现"Category_OC.h file not found"请在 Header Search Paths 中添加所在的文件夹目录
+
+#ifndef Category_OC_h
+#define Category_OC_h
+
+#import "AppDelegate+Notification.h"
+
+#import "NSString+Extension.h"
+#import "NSString+URLEncoding.h"
+#import "NSDate+Extension.h"
+
+#import "UIColor+HEX.h"
+#import "UITabBar+Badge.h"
+#import "UIView+Frame.h"
+#import "UIImage+Scale.h"
+#import "UIButton+Badge.h"
+#import "UIButton+Target.h"
+
+#endif /* Category_OC_h */
--- Category_OC/Category_OC/ViewController.m
+++ Category_OC/Category_OC/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-#import "UIColor+HEX.h"
+#import "Category_OC.h"
 
 @interface ViewController ()
 
